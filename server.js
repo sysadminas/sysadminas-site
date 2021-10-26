e2e7afa3bcea756771492d3d82d7c30f991871e7
Valesca Fortunato <valesca@gmail.com>
2021-10-26 11:18:36 -0300

    Update Server.Js

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+//const http = require('http');
+//const express = require('express');
+//const router = express.Router();
+
+//const path = require('path');
+//const app = express();
+//app.use(express.json());
+//app.use(express.static("app"));
+
+// default URL for website
+//app.use('/', function (req, res) {
+//  res.sendFile(path.join(__dirname + '/app/index.html'));
+  //__dirname : It will resolve to your project folder.
+//});
+
+//const server = http.createServer(app);
+//const port = 21164;
+//server.listen(port);
+
+//console.debug('Server listening on port ' + port);
+
+//
 const http = require('http');
 const express = require('express');
 const router = express.Router();
@@ -8,8 +30,8 @@ app.use(express.json());
 app.use(express.static("app"));
 
 // default URL for website
-app.use('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/app/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname,'./app/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
